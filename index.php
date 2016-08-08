0397c15308940930ea3c6a9baa60e34bcf15c8f8
ro <romain.pitz@gmail.com>
2016-08-08 17:32:41 +0800

    some more progress

--- index.php
+++ index.php
@@ -11,17 +11,28 @@ if (isset($_GET['cat'])) {
 }else{
     $cat_filter = "none";  
 }
+if (isset($_GET['post'])) {
+    $post_filter = $_GET['post'];
+}else{
+    $post_filter = "none";  
+}
 
 $categories = ['woodwork', 'shape', 'electronics', 'software'];
 $categories_links = ['woodwork' => 'Woodwork', 'shape'=>'Surfboard Shaping', 'electronics'=>'Electronics', 'software'=>'Software'];
 echo "<div id='header' class='header'>";
-echo "<div class='logo'>";
-echo "<div class='title font9'><a href='/blog/minimalist-blog/'>ROAST</a></div>";
-echo "<div class='subtitle'><a href='/blog/minimalist-blog/'>RO & SOME TOOLS</a></div>";
+echo "<div id='logo' class='logo'>";
+echo "<div id='title' class='title font9'><a href='/blog/minimalist-blog/'>ROAST</a></div>";
+echo "<div id='subtitle' class='subtitle'><a href='/blog/minimalist-blog/'>RO & SOME TOOLS</a></div>";
 echo "</div>";
-echo "<div class='header-links'>";
+echo "<div id='header-links' class='header-links'>";
+echo "<div class='top-link'><a href='.'>Home</a></div>";
 foreach ($categories as $cat){
-    echo "<div class='top-link'><a href='?cat=".$cat."'>".$categories_links[$cat]."</a></div>";
+        if ($cat_filter == $cat){
+            echo "<div class='top-link italic'><a href='?cat=".$cat."'>".$categories_links[$cat]."</a></div>";
+        }
+        else{
+            echo "<div class='top-link'><a href='?cat=".$cat."'>".$categories_links[$cat]."</a></div>";
+        }
 }
 echo "</div>";
 echo "</div>";
@@ -37,76 +48,101 @@ $posts_number = count($posts);
 $close = "no";
 
 $p=$posts[0];
+if (($cat_filter == 'none') && ($post_filter == 'none')) {
+    if (pathinfo($p, PATHINFO_EXTENSION)  == 'json'){
+        $post = json_decode(file_get_contents($directory.$p), true);
 
-if (pathinfo($p, PATHINFO_EXTENSION)  == 'json'){
-    $post = json_decode(file_get_contents($directory.$p), true);
-    if ($cat_filter == 'none'){
-            echo "<div class = 'first_post'>";
+        echo "<div class = 'first_post'>";
         
         echo "<img src='".$post['thumb']."'><br>";
         echo "<div class = 'category'>";
         echo "Latest Post: ";
-        echo $categories_links[$post['category']]."</div>";
-        echo "<div class = 'title'>".$post['title']."</div>";
-        echo "<div class = 'title'>".$post['id']."</div>";
+        echo "<a href='?cat=".$post['category']."'>".$categories_links[$post['category']]."</a></div>";
+        echo "<div class='first_post_card'>";
+        echo "<div class = 'title font4'>".$post['title']."</div>";
+        //echo "<div class = 'title'>".$post['id']."</div>";
         echo "<div class = 'abstract'>".$post['abstract']."</div>";
-        echo '<br>';
-        //echo $post['id'];
-
+        echo "<div class = 'date font2'>Posted on: ".substr($post['id'],6,2)."-".substr($post['id'],4,2)."-".substr($post['id'],0,4)."</div>";
+        echo '</div>';
        // echo "<div class = 'article'>".$post['article']."</div>" ;
         echo '</div>';
+        echo "<div class='void'></div>";
 
-    }
-    else{
-        if ($post['category'] == $cat_filter){
-            echo $post['title'];
-            echo "<br><img width=100px src='".$post['thumb']."'><br>";
-            echo $post['article'] ;
         }
     }
+$empty = 0;
 
-}
+if ($post_filter == 'none') {
+    for ($i=0;$i<3;$i++){
 
-for ($i=0;$i<3;$i++){
+            echo "<div class='column".$i."'>"; 
 
-        echo "<div class='column".$i."'>"; 
+         
+        for($j=0;3*$j+$i<$posts_number;$j++){
+            $p=$posts[3*$j+$i+1];
 
-     
-    for($j=0;3*$j+$i<$posts_number;$j++){
-        $p=$posts[3*$j+$i+1];
-
-        if (pathinfo($p, PATHINFO_EXTENSION)  == 'json'){
-            $post = json_decode(file_get_contents($directory.$p), true);
-            if ($cat_filter == 'none'){
-              
-                echo "<div class = 'post'>";
-                
-                echo "<img src='".$post['thumb']."'><br>";
-                echo "<div class = 'category'>";
-             
-                echo $categories_links[$post['category']]."</div>";
-                echo "<div class = 'title font4'>".$post['title']."</div>";
-                echo "<div class = 'abstract'>".$post['abstract']."</div>";
-                echo "<div class = 'date font2'>Posted on: ".substr($post['id'],6,2)."-".substr($post['id'],4,2)."-".substr($post['id'],0,4)."</div>";
+            if (pathinfo($p, PATHINFO_EXTENSION)  == 'json'){
+                $post = json_decode(file_get_contents($directory.$p), true);
+                if ($cat_filter == 'none'){
+                    $empty = 1;
+                    echo "<div class = 'post'>";
+                    echo "<a href='?post=".$post['id']."'>";
+                    echo "<img src='".$post['thumb']."'><br></a>";
+                    echo "<div class = 'category'>";
+                 
+                    echo "<a href='?cat=".$post['category']."'>".$categories_links[$post['category']]."</a></div>";
+                    echo "<a href='?post=".$post['id']."'>";
+                    echo "<div class = 'title font4'>".$post['title']."</div>";
+                    echo "<div class = 'abstract'>".$post['abstract']."</div>";
+                    echo "<div class = 'date font2'>Posted on: ".substr($post['id'],6,2)."-".substr($post['id'],4,2)."-".substr($post['id'],0,4)."</div>";
 
 
-               // echo "<div class = 'article'>".$post['article']."</div>" ;
-                echo '</div>';
-                echo "<div class='void'></div>";
-            }
-            else{
-                if ($post['category'] == $cat_filter){
-                    echo $post['title'];
-                    echo "<br><img width=100px src='".$post['thumb']."'><br>";
-                    echo $post['article'] ;
+                   // echo "<div class = 'article'>".$post['article']."</div>" ;
+                    echo '</a></div>';
+                    echo "<div class='void'></div>";
+                }
+                else{
+                    if ($post['category'] == $cat_filter){
+                        $empty = 1;
+                        echo "<div class = 'post'>";
+                        echo "<img src='".$post['thumb']."'><br>";
+                        echo "<div class = 'category'>";
+                        echo "<a href='?cat=".$post['category']."'>".$categories_links[$post['category']]."</a></div>";
+                        echo "<div class = 'title font4'>".$post['title']."</div>";
+                        echo "<div class = 'abstract'>".$post['abstract']."</div>";
+                        echo "<div class = 'date font2'>Posted on: ".substr($post['id'],6,2)."-".substr($post['id'],4,2)."-".substr($post['id'],0,4)."</div>";
+                        // echo "<div class = 'article'>".$post['article']."</div>" ;
+                        echo '</div>';
+                        echo "<div class='void'></div>";
+                    }
                 }
             }
         }
-
+        echo "</div>";
     }
-    echo "</div>";
+}
+else{
+    $post = json_decode(file_get_contents($directory.$post_filter.'.json'), true);
+    $empty = 1;
+    echo "<div class = 'post'>";
+    
+    echo "<img src='".$post['thumb']."'><br>";
+    echo "<div class = 'category'>";
+ 
+    echo "<a href='?cat=".$post['category']."'>".$categories_links[$post['category']]."</a></div>";
+    echo "<div class = 'title font4'>".$post['title']."</div>";
+    echo "<div class = 'abstract'>".$post['abstract']."</div>";
+    echo "<div class = 'date font2'>Posted on: ".substr($post['id'],6,2)."-".substr($post['id'],4,2)."-".substr($post['id'],0,4)."</div>";
+
+
+   // echo "<div class = 'article'>".$post['article']."</div>" ;
+    echo '</div>';
+    echo "<div class='void'></div>";
 }
 
+if ($empty == 0){
+    echo "Looks like I am gonna have to get things moving here!";
+}
 ?>
 <script >
     jQuery(document).ready(function() {
@@ -114,9 +150,15 @@ for ($i=0;$i<3;$i++){
             var scroll = $(window).scrollTop();
             if (scroll > 0) {
                 $("#header").addClass("header-shadow");
+                $("#header-links").height("3.75rem");
+                $("#title").css("font-size","2rem");
+                $("#subtitle").css("font-size","0.67rem");
             }
             else {
                 $("#header").removeClass("header-shadow");
+                $("#header-links").height("5.8rem");
+                $("#title").css("font-size","4rem");
+                $("#subtitle").css("font-size","1.355rem");
             }
         });
     });
